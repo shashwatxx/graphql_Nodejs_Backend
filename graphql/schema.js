6741a9b1d51638313cc21c384614832b2cb55138
shashwatxx <shashwatmishra2111@gmail.com>
2020-10-12 09:41:00 +0530

    auto

--- graphql/schema.js
+++ graphql/schema.js
@@ -26,6 +26,11 @@ type AuthData {
     userId:String!
 }
 
+type PostData {
+    posts:[Post!]!
+    totalPosts:Int!
+}
+
 input UserInputData {
     email: String!
     name:String!
@@ -40,6 +45,7 @@ input PostInputdata {
 
 type RootQuery {
     login(email:String!,password:String!): AuthData!
+    posts:PostData!
 }
 
 type RootMutation {
